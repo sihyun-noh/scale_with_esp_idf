a4a8946076074424940b8655f0a289dd29c6ce00
Jeff Yoon <jeyoon@outlook.com>
2022-12-08 13:11:15 +0900

    chore : Modify the build system to work more accurately   - Add target chip id to build firmware image based on chipset
    Signed-off-by: Jeff Yoon <jeyoon@outlook.com>

--- product/data_logger/build.sh
+++ product/data_logger/build.sh
@@ -27,6 +27,14 @@ checkArgVariable() {
 }
 
 if [ -z ${1} ] || [[ ${1} = "build" ]] || [[ ${1} = "flash" ]]; then
+  echo "Please select target chipset. 1: ESP32, 2: ESP32S3"
+  read CHIPSET_ID
+  if [ ${CHIPSET_ID} = "1" ]; then
+    export IDF_TARGET=esp32
+  elif [ ${CHIPSET_ID} = "2" ]; then
+    export IDF_TARGET=esp32s3
+  fi
+
   echo "Please select product. 1: Temp & Humi, 2 : CO2 TH, 3 : Soil EC, 4 : Solar Radiation, 5 : Atlas pH, 6 : Atlas EC, 7 : Water PH, 8 : Wind Direction, 9 : Wind Speed, 10 : Water EC"
   read SELECT_NUM
 
@@ -95,7 +103,7 @@ fi
 # tool setup & env setting
 cd ${TOOL_PATH}/esp-idf
 
-./install.sh esp32
+./install.sh all
 
 # exit 0
 . export.sh
@@ -113,7 +121,13 @@ clearEnvVariable() {
 checkDeviceConnect() {
   # 매개변수 없을 시에는 기본적으로 build 만 동작
   # Flash 일 경우 device 연결 확인 - 현재는 macOS, 추후 linux, window 확인 후 추가 예정
-  PORT_PATH=$(ls /dev/cu.usb*)
+  # check if fuzzy finder command exists
+  which fzf &>/dev/null
+  if [ $? == 0 ] ; then
+    PORT_PATH=$(ls /dev/cu.usb* | fzf)
+  else
+    PORT_PATH=$(ls /dev/cu.usb*)
+  fi
   echo "port port : $PORT_PATH"
 
   if [ -z ${PORT_PATH} ]; then
