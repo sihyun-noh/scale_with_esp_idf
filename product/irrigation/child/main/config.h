4d7df3a0c275ab5162829c482f9d19b8f1b54282
Dongsu Kim <dongsu.kim@greenlabs.co.kr>
2023-01-30 11:10:47 +0900

    feat: Update config.h of irrigation product
    Signed-off-by: Dongsu Kim <dongsu.kim@greenlabs.co.kr>

--- product/irrigation/child/main/config.h
+++ product/irrigation/child/main/config.h
@@ -4,15 +4,6 @@
 /******************************************
  * Board : ESP32
  *****************************************/
-#define MASTER 1
-#define HID    2
-#define CHILD  3
-
-#define IRRIGATION_TYPE CHILD
-
-#define SENSOR_TYPE     -1
-#define ACTUATOR_TYPE   -1
-
 #define BATTERY_ADC_CHANNEL  6  // GPIO_34
 #define BATTERY_READ_ON_GPIO 5  // GPIO_5
 
--- product/irrigation/hid/main/config.h
+++ product/irrigation/hid/main/config.h
@@ -4,22 +4,7 @@
 /******************************************
  * Board : ESP32S3
  *****************************************/
-#define MASTER 1
-#define HID 2
-#define CHILD 3
-
-#define IRRIGATION_TYPE HID
-
-#define SENSOR_TYPE -1
-#define ACTUATOR_TYPE -1
-
 #define BATTERY_PORT 6  // GPIO34
-
-// #define SDCARD_SPI_MISO 2
-// #define SDCARD_SPI_MOSI 15
-// #define SDCARD_SPI_CLK 14
-// #define SDCARD_SPI_CS 13
-// #define SDCARD_SPI_DMA_CHAN 1
 
 #define SDSPI_HOST_ID SPI3_HOST
 #define SD_MISO GPIO_NUM_38
